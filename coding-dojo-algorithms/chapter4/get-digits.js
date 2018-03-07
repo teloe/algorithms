6b731406964eb5bafd6651757bfebc7654f38972
Tom Eloe <tomeloe@gmail.com>
2018-03-07 10:03:22 -0800

    more algos

--- coding-dojo-algorithms/chapter4/get-digits.js
+++ coding-dojo-algorithms/chapter4/get-digits.js
@@ -1,14 +1,7 @@
 // Create a JavaScript function that given a string, returns the integer made from the string’s digits. 
 // Given "0s1a3y5w7h9a2t4?6!8?0" , the function should return the number 1357924680
 
-function getDigits(str) {
-  var arr = [], x = str.split('');
-  for (var i = 0; i < arr.length; i++) {
-    if (typeof arr[i] == "number") {
-      arr.push(i);
-    }
-  }
-  console.log(arr);
-}
-
-getDigits("a1b2c3");
+var txt = "0s1a3y5w7h9a2t4?6!8?0";
+var numb = txt.match(/\d/g);
+numb = numb.join("");
+console.log(numb);

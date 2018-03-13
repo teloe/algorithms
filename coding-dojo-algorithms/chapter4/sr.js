f4d0254c622c1d25f657fac67e86017d6d53e148
Tom Eloe <tomeloe@gmail.com>
2018-03-13 11:09:19 -0700

    more algos

--- coding-dojo-algorithms/chapter4/sr.js
+++ coding-dojo-algorithms/chapter4/sr.js
@@ -1,21 +1,13 @@
-// Implement reverseString(str) that, given string, returns that string with characters reversed. Given "creature" , return "erutaerc" . Tempting as it seems, do not use the built-in reverse()!
+// Implement reverseString(str) that, given string, returns that string with characters reversed.
+// Given "creature" , return "erutaerc" .
 
-var str = "string";
+var str = "hello";
 
 function stringReverse(str) {
-  var a = str.split('');
-  var i = 0;
-  var j = a.length - 1;
-  while (i < j) {
-    var temp = a[i];
-    a[i] = a[j];
-    a[j] = temp;
-    i++;
-    j--;  
-  }
-  var newStr = a.join();
-  console.log(newStr);
+  var arr = str.split('');
+  var reverseArr = arr.reverse();
+  var join = reverseArr.join();
+  console.log(join);
 }
 
 stringReverse(str);
-

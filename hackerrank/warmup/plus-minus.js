eec7e19674981a12b2a411d884cd0e75af453c8b
Tom Eloe <tomeloe@gmail.com>
2018-09-06 16:45:11 -0700

    corrected plus-minus.js

--- hackerrank/warmup/plus-minus.js
+++ hackerrank/warmup/plus-minus.js
@@ -1,22 +1,19 @@
 function plusMinus(arr) {
-  var p = 0;
-  var z = 0;
-  var n = 0;
-  for (var i = 0; i < arr.length; i++) {
+  var p = 0, z = 0, n = 0, num = arr.length;
+  for (var i = 0; i < num; i++) {
     if (arr[i] > 0) {
       // increment p
-      p += 1;
+      p++;
     } else if (arr[i] == 0) {
       // increment z
-      z += 1;
+      z++;
     } else {
       // increment n
-      n += 1;
+      n++;
     }
   }
-  return p/arr.length, z/arr.length, n/arr.length;
+  return [p/num, n/num, z/num];
 }
 
-var arr = [1, 1, 0, -1, -1];
-console.log(plusMinus(arr));
-
+var arr = [-4, 3, -9, 0, 4, 1];
+console.log(plusMinus(arr)); // [ 0.5, 0.3333333333333333, 0.16666666666666666 ] 

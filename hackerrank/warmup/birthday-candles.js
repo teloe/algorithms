ae99534224d8c591066af751d096827e3a052c56
Tom Eloe <tomeloe@gmail.com>
2018-09-10 09:23:40 -0700

    updated birthday-candles.js

--- hackerrank/warmup/birthday-candles.js
+++ hackerrank/warmup/birthday-candles.js
@@ -1,9 +1,17 @@
-function birthdayCakeCandles(ar) {
-  let num = ar.length;
-  for (let i = 0; i < num; i++) {
+function birthdayCakeCandles(n, ar) {
+  let num = 0;
+  let largest = 0;
+  for (let i = 0; i < n; i++) {
+    if (ar[i] > num) {
+      num = ar[i];
+      largest = 1;
+    } else if (ar[i] == num) {
+      largest++;
+    }
   }
+  console.log(largest);
 
 }
 
-let ar = [3, 2, 1, 3];
-birthdayCakeCandles(ar);
+let ar = [3, 2, 1, 3], n = 4;
+birthdayCakeCandles(n, ar); // 2

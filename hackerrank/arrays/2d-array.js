91e531f4c0d4b01c98d556eff86e7368f2c527d8
Tom Eloe <tomeloe@gmail.com>
2018-09-12 22:01:07 -0700

    more algos

---
+++ hackerrank/arrays/2d-array.js
@@ -0,0 +1,11 @@
+/*
+ * Given a 6 x 6 2d array, arr: calculate the hourglass sum for every hourglass in arr, then print the maximum hourglass
+ * sum.
+ *
+ */
+
+function hourglassSum(arr) {
+  let 
+}
+
+console.log(hourglassSum(arr));

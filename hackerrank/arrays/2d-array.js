51343d90f25351572e6ef5bbb9cb6956f7b28ee3
Tom Eloe <tomeloe@gmail.com>
2018-09-14 07:55:38 -0700

    updated 2d-array.js

--- hackerrank/arrays/2d-array.js
+++ hackerrank/arrays/2d-array.js
@@ -12,67 +12,18 @@ let arr = [
   [ 0, 0, 1, 2, 4, 0 ]
 ];
 
-// Output should be 19 
-
-'use strict';
-
-const fs = require('fs');
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', _ => {
-    inputString = inputString.replace(/\s*$/, '')
-        .split('\n')
-        .map(str => str.replace(/\s*$/, ''));
-
-    main();
-});
-
-function readLine() {
-    return inputString[currentLine++];
+const hourglassSum = arr => {
+  let sum = []
+  for (i = 0; i < 3; i++) {
+    for (j = 0; j < 3; j++){
+      sum.push(
+        (arr[i][j]+arr[i][j+1]+arr[i][j+2]) + 
+        (arr[i+1][j+1]) + 
+        (arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2])
+      )
+    }
+  }
+  console.log(Math.max.apply(null, sum));
 }
 
-// Complete the hourglassSum function below.
-function hourglassSum(arr) {
-  var arr = [], count = -63;
-    for(arr_i = 0; arr_i < 6; arr_i++){
-      arr[arr_i] = readLine().split(' ');
-      arr[arr_i] = arr[arr_i].map(Number);
-    }
-    for( i = 0 ; i < 4 ; i ++) {
-      for( j = 0 ; j < 4 ; j ++) {
-        var t;
-        t = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
-        if( t > count ) {
-          count = t ;
-        }
-      }
-    }
-    console.log(count);
-}
-
-function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-    let arr = Array(6);
-
-    for (let i = 0; i < 6; i++) {
-        arr[i] = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
-    }
-
-    let result = hourglassSum(arr);
-
-    ws.write(result + "\n");
-
-    ws.end();
-}
-
-hourglassSum();
+hourglassSum(arr);

322a88235f652557765f6a18fd94227a50ad67f2
Tom Eloe <tomeloe@gmail.com>
2018-12-03 08:49:39 -0800

    add strong-password.js

--- hackerrank/strings/strong-password.js
+++ hackerrank/strings/strong-password.js
@@ -11,7 +11,7 @@ function minimumNumber(n, p) {
   const c = [/[0-9]/, /[a-z]/, /[A-Z]/, /[!@#$%^&*()\-+"]/]
             .map(r => !r.test(p))
             .filter(Boolean).length
-    return Math.max(c,6-n); 
+  return Math.max(c,6-n); 
 }
 
 console.log(minimumNumber(3, 'Ab1')); // 3

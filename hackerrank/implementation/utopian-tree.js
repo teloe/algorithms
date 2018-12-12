62c85a7aacf46708f1a2f3149dcf4bca4e1611b9
Tom Eloe <tomeloe@gmail.com>
2018-12-12 10:37:45 -0800

    add uptopian-tree.js

---
+++ hackerrank/implementation/utopian-tree.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * HackerRank - Utopian Tree
+ * https://www.hackerrank.com/challenges/utopian-tree/problem
+ * 
+ * Complete the utopianTree function in the editor below. 
+ * It should return the integer height of the tree after the input number of growth cycles.
+ */
+
+function utopianTree(n) {
+  let currentHeight = 1;
+  for (let i = 1; i <= n; i++) {
+    if (currentHeight % 2 !== 0) {
+      currentHeight *= 2;
+    } else {
+      currentHeight += 1;
+    }
+  }
+  return currentHeight;
+}
+
+console.log(utopianTree(5)); // 14

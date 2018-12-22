98f5e73a081909018381d12b949a2849214a9c8f
Tom Eloe <tomeloe@gmail.com>
2018-12-21 16:03:43 -0800

    add save-the-prisoner.js

---
+++ hackerrank/implementation/save-the-prisoner.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * HackerRank - Save The Prisoner!
+ * https://www.hackerrank.com/challenges/save-the-prisoner/problem
+ * 
+ * Complete the saveThePrisoner function in the editor below. 
+ * It should return an integer representing the chair number of the prisoner to warn.
+ */
+
+function saveThePrisoner(n, m, s) {
+  // starting at seat, s and counting up to m
+  // if chair reaches n prisoners, then reset count back to seat 1 and continue to count
+  // once count reaches m, end and return
+  let count = ((m % n) + s) - 1;
+  return count;
+}
+
+console.log(saveThePrisoner(5, 2, 1)); // 2
+console.log(saveThePrisoner(5, 2, 2)); // 3
+console.log(saveThePrisoner(7, 19, 2)); // 6
